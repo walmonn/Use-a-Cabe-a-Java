d7c9ffd349ae9d9747616cc1b1aa6f8e32da6236
walmonn <wagner.a.monte@hotmail.com>
2023-12-24 14:43:53 -0300

    commit_02

---
+++ shuffle1.java
@@ -0,0 +1,22 @@
+class shuffle1 {
+    public static void main (String [] args) {
+
+    int x = 3;
+    if ( x > 2 ) {
+      System.out.print ("a");
+}
+    
+    while (x > 0){   
+    x = x - 1;
+    System.out.print("_");
+
+   if (x == 2) {
+      System.out.print("b c");
+
+}   if (x == 1) {
+      System.out.print("d");
+         x = x - 1;    
+}
+}
+}
+}

2691991e388aac8a52e50c3cd82c17222981d792
walmonn <wagner.a.monte@hotmail.com>
2024-01-18 17:23:14 -0300

    commit_00

---
+++ candidato_2.java
@@ -0,0 +1,17 @@
+class Test {
+   public static void main (String [] args) {
+      int x = 0;
+      int y = 0;
+      while (x < 5) {
+	 y = x + x;
+	 
+
+
+	 System.out.print(x + " " + y + " ");
+	 x = x + 1;
+
+  }     	
+
+ }
+
+}

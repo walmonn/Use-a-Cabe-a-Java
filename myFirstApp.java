b1b61d1e4f65dfaeb75503e3859e4c421daf3f52
walmon <wagner.a.monte@hotmail.com>
2025-01-17 21:58:57 -0300

    commit_001

--- myFirstApp.java
+++ myFirstApp.java
@@ -3,6 +3,6 @@ public class myFirstApp {
 	public static void main (String[] args) {
 
 	System.out.println("hello world");
-	System.out.println("My name is Wagner");
+	System.out.println("My name is Wagner Alves do Monte");
  }
 }
